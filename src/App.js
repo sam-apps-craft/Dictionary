1e1e9180d9cbea576c89b91e09cacddfd93e7c74
sam1246 <sam@apps-craft.com>
2024-03-19 15:39:24 +0000

    search form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import logo from "./logo.png";
+import logo2 from "./logo2.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer text-center">
+          <p className="">
+            Coded by{" "}
+            <a href="https://www.apps-craft.com/" target="blank">
+              <img
+                src={logo2}
+                className="App-logo-footer img-fluid rounded"
+                alt="logo2"
+              />
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
-
- 
