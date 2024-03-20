20a170f3a7283f25ba69bd33630ebe622b7a3da3
sam1246 <sam@apps-craft.com>
2024-03-20 12:39:59 +0000

    axios and api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data.word);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+    
+
+    let key = "6a33dd4o00tf5d96902aafb8b01808f5";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
