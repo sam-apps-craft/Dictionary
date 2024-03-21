99619793d405ad1f9a27f73974d6add5668c55cc
sam1246 <sam@apps-craft.com>
2024-03-21 18:38:42 +0000

    rendered word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data.word);
+    setResults(response.data);
   }
   function search(event) {
     event.preventDefault();
-    
 
     let key = "6a33dd4o00tf5d96902aafb8b01808f5";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
@@ -25,6 +26,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
---
+++ src/Meaning.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return (
+  <div className="Meaning">
+  <h3>{props.meaning.definition}</h3>;
+  <p>{props.meaning.definition}</p>
+  <em>{props.meaning.example}</em>
+  </div>
+  );
+}
