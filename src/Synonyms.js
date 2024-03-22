db17f1f0a4bbdd082ab324a6474854d21a86b55e
sam1246 <sam@apps-craft.com>
2024-03-22 14:47:35 +0000

    correct mistake

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 export default function Synonyms(props) {
+    console.log(props.synonyms);
   if (props.synonyms) {
     return (
       <ul className="Synonyms">
-        {props.synonyms.map(function (synonym, index) {
+        {props.results.synonyms.map(function (synonym, index) {
           return <li key={index}>{synonym}</li>;
         })}
       </ul>
